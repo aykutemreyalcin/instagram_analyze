519de6ae4809e75496a438d3307f0a9cef52c767
Aykut <76070532+aykutemreyalcin@users.noreply.github.com>
2024-06-26 08:05:09 -0500

    add get_followees function to main.py

--- main.py
+++ main.py
@@ -14,3 +14,15 @@ def get_followers_list(username,password):
         follower_count += 1
     print(f"you have {follower_count} followers")
     return follower_list
+
+def get_followees_list(username,password):
+    L = instaloader.Instaloader()
+    profile = instaloader.Profile.from_username(L.context, "prada")
+    followees_list = []
+    followees_count = 0
+    for followee in profile.get_followees():
+        followees_list.append(followee.username)
+        followees_count += 1
+    print(f"you follow {followees_count} accounts")
+    return followees_list
+
